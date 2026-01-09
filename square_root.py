ce12c35dbd306037776dbd7d89aa31dd3ca4d51c
suhailmajeed-pixel <suhailmajeed@gmail.com>
2026-01-09 16:02:59 +0530

    Add files via upload

---
+++ square_root.py
@@ -0,0 +1,3 @@
+number = float(input("Enter a number: "))
+square_root = number ** 0.5
+print("Square root is:", square_root)
---
+++ summer_times.py
@@ -0,0 +1,6 @@
+temperature = int(input("Enter the temperature in Celsius: "))
+
+if temperature >= 25:
+    print("You can wear light clothes.")
+else:
+    print("Better wear warm clothes.")
