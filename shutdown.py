e71ecb664c01df63b87075dd1e53e6ec48925b49
suhailmajeed-pixel <suhailmajeed@gmail.com>
2026-03-05 22:16:26 +0530

    Add files via upload

--- shutdown.py
+++ shutdown.py
@@ -1,9 +1,19 @@
 def shutdown(choice):
     if choice == "YES":
-        print("Shutting down")
-    elif choice == "NO":
-        print("Abort shutdown")
-    else:
-        print("Sorry write in capital")
+        print("!!!Shutting down!!!")
 
-shutdown(input("Shutdown? (Yes/No): "))
+    elif choice == "NO":
+        print("OK! abort the shutdown ")
+   
+    elif choice == "yes":
+        print("PLEASE WRITE IN CAPITAL")
+
+    elif choice == "no":
+        print("PLEASE IN CAPITAL")
+
+    else:
+        print("ONLY WRITE YES OR NO")
+
+user_choice = input("Do you want to shutdown the system? (Yes/No): ")
+
+shutdown(user_choice)
