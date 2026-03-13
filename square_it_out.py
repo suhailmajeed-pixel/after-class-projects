a311de1f3f591e0b79ee0bec9582ef80bd9ae21d
suhailmajeed-pixel <suhailmajeed@gmail.com>
2026-03-13 18:56:32 +0530

    Add files via upload

---
+++ square_it_out.py
@@ -0,0 +1,10 @@
+start = int(input("Enter start: "))
+end = int(input("Enter end: "))
+
+squares = [i**2 for i in range(start, end+1)]
+even = [x for x in squares if x % 2 == 0]
+odd = [x for x in squares if x % 2 != 0]
+
+print("Squares:", squares)
+print("Even Squares:", even)
+print("Odd Squares:", odd)
---
+++ tuple_product.py
@@ -0,0 +1,8 @@
+t = (2, 3, 4, 5)
+
+product = 1
+
+for i in t:
+    product = product * i
+
+print("Product of all numbers in the tuple is:", product)
